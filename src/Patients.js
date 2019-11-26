6b3137d0c1240d72789b158d135c6b5cbe3f1c88
keerthi <velaga88@gmail.com>
2019-11-26 10:38:48 -0500

    css for patients

--- src/Patients.js
+++ src/Patients.js
@@ -1,72 +1,92 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+//import './patient.css'
 // import './App.css';
-const patientUrl = 'http://localhost:3000/api';
-class Patients extends React.Component{
-    state = {
-        patients: [],
-        newPatient: {
-            name: "",
-            age: "",
-            gender: null,
-            patientId: null
-        }
+const patientUrl = "http://localhost:3000/api";
+class Patients extends React.Component {
+  state = {
+    patients: [],
+    newPatient: {
+      name: "",
+      age: "",
+      gender: ""
     }
-    componentDidMount(){
-        this.getPatients();
-    }
-    getPatients = () => {
-        axios({
-          url: `${patientUrl}/patients`,
-          method: "get"
-        }).then(response => {
-          this.setState({
-            patients: response.data.patients
-          });
-        console.log(response);
-        });
-      };
-      createPatient = e => {
-        e.preventDefault();
-        axios({
-          url: `${patientUrl}/pets`,
-          method: "post",
-          data: { newPet: this.state.newPet }
-        }).then(response => {
-          this.setState(prevState => ({
-            pets: [...prevState.pets, response.data.pet]
-          }));
-        });
-      };
-      deletePatient = e => {
-        axios({
-          url: `${patientUrl}/patients/${e.target.id}`,
-          method: "delete"
-        }).then(response => {
-          this.setState({ patients: response.data.patients });
-        });
-      };
+  };
+  componentDidMount() {
+    this.getPatients();
+  }
+  getPatients = () => {
+    axios({
+      url: `${patientUrl}/patients`,
+      method: "get"
+    }).then(response => {
+      this.setState({
+        patients: response.data.patients
+      });
+      console.log(response);
+    });
+  };
+  createPatient = e => {
+    e.preventDefault();
+    axios({
+      url: `${patientUrl}/patients`,
+      method: "post",
+      data: { newPatient: this.state.newPatient }
+    }).then(response => {
+      //   this.setState(prevState => ({
+      //     patients: [...prevState.patients, response.data.patients]
+      //   }));
+      this.setState({ patients: response.data.patients });
+    });
+  };
+  handleChange = e => {
+    let newPatient = {
+      [e.target.name]: e.target.value,
+      [e.target.age]: e.target.value,
+      [e.target.gender]: e.target.value
+    };
+    this.setState((prevState, currentState) => ({
+      newPatient: { ...prevState.newPatient, ...newPatient }
+    }));
+  };
+  deletePatient = e => {
+    axios({
+      url: `${patientUrl}/patients/${e.target.id}`,
+      method: "delete"
+    }).then(response => {
+      this.setState({ patients: response.data.patients });
+    });
+  };
       render() {
         console.log(this.state);
         const patientEls = this.state.patients.map(patient => {
           return (
-            <li key={patient.id}>
-              {patient.name} -- {patient.age} -- {patient.gender} -- {patient.patientId}
-              <button id={patient.id} onClick={this.deletePatient}>
-                Delete Patient
-              </button>
-              <button id={patient.id} onClick={this.createPatient}>
-                Create Patient
-              </button>
-            </li>
+            
+            <div key={patient.id}  className="patientsCardDiv">
+                <p>Name: {patient.name}</p>
+                <p>Age: {patient.age}</p>
+                <p>Gender: {patient.gender}</p>
+                <p>ID: {patient.id}</p>
+                <i class="material-icons md-dark" id={patient.id} onClick={this.deletePatient}>delete</i>
+            </div>
+            
+            // <li key={patient.id}>
+            //   {patient.name} -- {patient.age} -- {patient.gender} -- {patient.patientId}
+            //   <button id={patient.id} onClick={this.deletePatient}>
+            //     Delete Patient
+            //   </button>
+            //   <button id={patient.id} onClick={this.createPatient}>
+            //     Update Patient
+            //   </button>
+            // </li>
           );
         });
         return(
-            <div id="PatientsDiv">
-               {/* <ul>{patientEls}</ul> */}
+            <div className="patientContainer">
+               <i class="material-icons md-dark" id="addButton">add_circle</i>
+              <ul>{patientEls}</ul> 
             </div>
         )
     }
 }
 export default Patients;
-
